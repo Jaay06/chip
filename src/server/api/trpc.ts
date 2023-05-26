d049d13efc0c033e2c455ebfa5c1a76e66fa8194
Jaay06 <josekoyi9@gmail.com>
2023-05-26 02:51:15 +0100

    Routing

--- src/server/api/trpc.ts
+++ src/server/api/trpc.ts
@@ -47,7 +47,6 @@ import { TRPCError, initTRPC } from "@trpc/server";
 import superjson from "superjson";
 import { ZodError } from "zod";
 import { getAuth } from "@clerk/nextjs/server";
-import { error } from "console";
 
 const t = initTRPC.context<typeof createTRPCContext>().create({
   transformer: superjson,
